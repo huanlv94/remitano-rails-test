3a27a18e66565f81b1468110b47f27d2a5695fdd
lehuan94cntt <lehuan94cntt@gmail.com>
2021-07-13 16:43:40 +0700

    FE: static template for homepage

--- app/javascript/packs/home.jsx
+++ app/javascript/packs/home.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 const Hello = props => (
-  <div>Hello {props.data.message}!</div>
+  <div className='share-container'>
+    {_.times(10, (i) => (
+      <div className='share-content'>
+        <div className='share-preview embed-responsive'>
+          <iframe width = '39%' className="embed-responsive-item" src="https://www.youtube.com/embed/kGT73GcwhCU?rel=0" allowFullScreen></iframe>
+          <div className='share-description' width='60%'>
+            <h5>Video title</h5>
+            <p className='no-margin'>Share by: huan@gmail.com</p>
+            <p className='no-margin'>
+              <span className="bi bi-hand-thumbs-up"></span>
+              <span>69</span>
+              <span className="bi bi-hand-thumbs-down"></span>
+              <span>1</span>
+             </p>
+            <p className='no-margin'> Description:</p>
+            <p>as cj allowfullscreen
+            asc jasc ajscha
+            ascn asjcashjcbasc
+            jascjascbhascb
+            </p>
+          </div>
+        </div>
+      </div>
+    ))}
+    <div className='share-loadmore'>
+      <button className='btn btn-primary full-width'>Load more</button>
+    </div>
+  </div>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
